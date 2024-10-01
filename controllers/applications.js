00a850c9835bca16a9119a5810ab653cfec4b5b5
Rani Bungay <ranibungay@gmail.com>
2024-10-01 12:24:52 -0400

    added delete stage

--- controllers/applications.js
+++ controllers/applications.js
@@ -58,4 +58,22 @@ router.get("/:applicationId", async (req, res) => {
   }
 });
 
+router.delete("/:applicationId", async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Use the Mongoose .deleteOne() method to delete
+    // an application using the id supplied from req.params
+    currentUser.applications.id(req.params.applicationId).deleteOne();
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;
