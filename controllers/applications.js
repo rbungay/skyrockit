56a4a30a6b3c108c8a81348311892916898372ce
Rani Bungay <ranibungay@gmail.com>
2024-10-01 12:20:52 -0400

    completed shows page

--- controllers/applications.js
+++ controllers/applications.js
@@ -5,8 +5,15 @@ const User = require("../models/user.js");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("applications/index.ejs");
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Render index.ejs, passing in all of the current user's
+    // applications as data in the context object.
+    res.render("applications/index.ejs", {
+      applications: currentUser.applications,
+    });
   } catch (error) {
+    // If any errors, log them and redirect back home
     console.log(error);
     res.redirect("/");
   }
@@ -14,6 +21,41 @@ router.get("/", async (req, res) => {
 
 router.get("/new", async (req, res) => {
   res.render("applications/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:applicationId", async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the application by the applicationId supplied from req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Render the show view, passing the application data in the context object
+    res.render("applications/show.ejs", {
+      application: application,
+    });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
